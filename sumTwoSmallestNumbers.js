b653478c1bb3f534d2829bc288aa634a8bb89d92
Albert <albert.barsamov@gmail.com>
2021-11-13 20:44:43 +0100

    refactoring

--- sumTwoSmallestNumbers.js
+++ sumTwoSmallestNumbers.js
@@ -5,12 +5,10 @@ For example, when an array is passed like [19, 5, 42, 2, 77], the output should 
 [10, 343445353, 3453445, 3453545353453] should return 3453455.
 */
 
-function sumTwoSmallestNumbers(numbers) {
-  const min = numbers
+const sumTwoSmallestNumbers = (numbers) =>
+  numbers
     .sort((a, b) => a - b)
     .slice(0, 2)
     .reduce((x, y) => x + y);
-  return min;
-}
 
-console.log(sumTwoSmallestNumbers([19, 5, 42, 2, 77]));
+console.log(sumTwoSmallestNumbers([19, 5, 42, 2, 77])); // 7
